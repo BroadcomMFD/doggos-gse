d831bbd233a25d0849f6103343330dffb5aed093
Boris Petkov <boris.petkov@broadcom.com>
2023-03-26 18:31:00 +0200

    Update config task

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 set -e
-printf "\nConfiguring Test4z...\n\n"
-npx zowe config set profiles.lpar1.profiles.test4z.properties.hlq "$1"
-npx zowe config set profiles.lpar1.profiles.test4z.properties.user "$1"
-npx zowe config set profiles.lpar1.profiles.test4z.properties.password "$1"
-npx zowe config set profiles.lpar1.profiles.zosmf.properties.user "$1"
-npx zowe config set profiles.lpar1.profiles.zosmf.properties.password "$1"
-printf "\n#### Test4z configuration successful ####"
+printf "\nConfiguring Zowe CLI...\n\n"
+sed -i -e 's/TESTID##/${1}/g' zowe.config.json
+sed -i -e 's/TESTID##/${1}/g' .vscode/tasks.json
+# npx zowe config set profiles.lpar1.profiles.test4z.properties.hlq "$1"
+# npx zowe config set profiles.lpar1.profiles.test4z.properties.user "$1"
+# npx zowe config set profiles.lpar1.profiles.test4z.properties.password "$1"
+# npx zowe config set profiles.lpar1.profiles.zosmf.properties.user "$1"
+# npx zowe config set profiles.lpar1.profiles.zosmf.properties.password "$1"
+# npx zowe config set profiles.lpar1.profiles.endevor.properties.user "$1"
+# npx zowe config set profiles.lpar1.profiles.endevor.properties.password "$1"
+printf "\n#### Zowe CLI configuration successful ####"
